25895c93b7a86dcd4a1e2e8c992190021136a800
Alex. Hannel <support@syntaxxx.at>
2016-06-13 17:38:57 +0200

    CLEAN UP

--- _config.php
+++ _config.php
@@ -2,6 +2,6 @@
 /**
  * This is the config file of the SilverStripe ColorPicker Form-Element
  * @author Roman Schmid, AKA banal
- * 
+ *
  * ColorPicker jQuery widget/plugin by http://www.eyecon.ro/colorpicker/
  */
